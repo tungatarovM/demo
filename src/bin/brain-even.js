6c2a3456b01e3e3ad55087f050b3148cc6bb27ed
Tungatarov96 <tungatarov.96@mail.ru>
2018-02-14 00:40:23 +0500

    double execution of auth func problem is fixed

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 import authentification from '../index';
-import isEvenGame from '../isEven';
 
-isEvenGame(authentification());
+
+authentification();
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import readlineSync from 'readline-sync';
+import isEvenGame from './isEven';
 
 
 const authentification = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hi ${userName}`);
-  return userName;
+  isEvenGame(userName);
 };
 
 export default authentification;
