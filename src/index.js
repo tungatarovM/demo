fbbd152707f3aaa7b8c915f81b40ed1b8212fc52
Tungatarov96 <tungatarov.96@mail.ru>
2018-02-13 12:24:19 +0500

    checkpoint #3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const startFunc = () => {
-    console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hi ${userName}`);
+const startFunc = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hi ${userName}`);
 };
+export default startFunc;
